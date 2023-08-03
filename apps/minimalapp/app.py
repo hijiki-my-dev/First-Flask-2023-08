4c8a4f5c4c16954522895bc7a256ade785e4b1c7
Hibiki-Ich <hijikiblog01@gmail.com>
2023-08-03 12:59:02 +0900

    before request form

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, current_app, g, request
 
 app = Flask(__name__)
 
@@ -20,7 +20,13 @@ def show_name(name):
     return render_template("index.html", name=name)
 
 
-with app.test_request_context():
-    print(url_for("index"))
-    print(url_for("hello-endpoint", name="world"))
-    print(url_for("show_name", name="mito", page="1"))
+with app.test_request_context("/users?updated=true"):
+    print(request.args.get("updated"))
+    # print(url_for("index"))
+    # print(url_for("hello-endpoint", name="world"))
+    # print(url_for("show_name", name="mito", page="1"))
+
+ctx = app.app_context()
+ctx.push()
+
+print(g.connection)
