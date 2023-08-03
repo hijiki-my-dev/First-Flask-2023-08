b08a13f98a1cda285673b646f8bf67805b630621
Hibiki-Ich <hijikiblog01@gmail.com>
2023-08-03 11:52:52 +0900

    until url_for

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
+# route1つが1つのWebページに対応するイメージ？
 @app.route("/")
 def index():
     return "Hello, Flaskbook!"
@@ -11,3 +12,15 @@ def index():
 @app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    # 変数をテンプレートエンジンに渡す
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("hello-endpoint", name="world"))
+    print(url_for("show_name", name="mito", page="1"))
