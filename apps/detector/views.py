0ee048a5f15f1596d820275d2aa58f0ed0173a49
Hibiki-Ich <hijikiblog01@gmail.com>
2023-08-10 23:56:46 +0900

    画像アップ時の処理、保存を追加

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,8 +1,20 @@
+import uuid
+from pathlib import Path
+
 from apps.app import db
 from apps.crud.models import User
 from apps.detector.models import UserImage
 
-from flask import Blueprint, render_template, current_app, send_from_directory
+from flask import (
+    Blueprint,
+    render_template,
+    current_app,
+    send_from_directory,
+    redirect,
+    url_for,
+)
+
+from flask_login import current_user, login_required
 
 dt = Blueprint("detector", __name__, template_folder="templates")
 
@@ -23,3 +35,27 @@ def index():
 @dt.route("/images/<path:filename>")
 def image_file(filename):
     return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
+
+
+@dt.route("/upload", methods=["GET", "POST"])
+@login_required
+def upload_image():
+    form = UploadImageForm()
+    if form.validate_on_submit():
+        # アップされた画像データを取得
+        file = form.image.data
+        # ファイル名と拡張子を取得、UUIDに変換する（セキュリティー）
+        ext = Path(file.filename).suffix
+        image_uuid_file_name = str(uuid.uuid4()) + ext
+
+        # 画像を保存
+        image_path = Path(current_app.config["UPLOAD_FOLDER"], image_uuid_file_name)
+        file.save(image_path)
+
+        # DBに保存
+        user_image = UserImage(user_id=current_user.id, image_path=image_uuid_file_name)
+        db.session.add(user_image)
+        db.session.commit()
+
+        return redirect(url_for("detector.index"))
+    return render_template("detector/upload.html", form=form)
