9d12d05308ee9dcbd37f86954c9e6ba797449235
Hibiki-Ich <hijikiblog01@gmail.com>
2023-08-11 20:35:49 +0900

    物体検知のメイン部分を追加。画像の表示サイズが馬鹿でかいエラーが解決できない

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,10 +1,16 @@
+import random
+import cv2
+import numpy as np
+import torch
+import torchvision
+
 import uuid
 from pathlib import Path
 
 from apps.app import db
 from apps.crud.models import User
-from apps.detector.models import UserImage
-from apps.detector.forms import UploadImageForm
+from apps.detector.models import UserImage, UserImageTag
+from apps.detector.forms import UploadImageForm, DetectorForm
 
 from flask import (
     Blueprint,
@@ -13,7 +19,11 @@ from flask import (
     send_from_directory,
     redirect,
     url_for,
+    flash,
 )
+
+from PIL import Image
+from sqlalchemy.exc import SQLAlchemyError
 
 from flask_login import current_user, login_required
 
@@ -30,7 +40,26 @@ def index():
         .filter(User.id == UserImage.user_id)
         .all()
     )
-    return render_template("detector/index.html", user_images=user_images)
+
+    # タグ一覧を取得
+    user_image_tag_dict = {}
+    for user_image in user_images:
+        user_image_tags = (
+            db.session.query(UserImageTag)
+            .filter(UserImageTag.user_image_id == user_image.UserImage.id)
+            .all()
+        )
+        user_image_tag_dict[user_image.UserImage.id] = user_image_tags
+
+    # 物体検知フォームをインスタンス化
+    detector_form = DetectorForm()
+
+    return render_template(
+        "detector/index.html",
+        user_images=user_images,
+        user_image_tag_dict=user_image_tag_dict,
+        detector_form=detector_form,
+    )
 
 
 @dt.route("/images/<path:filename>")
@@ -60,3 +89,125 @@ def upload_image():
 
         return redirect(url_for("detector.index"))
     return render_template("detector/upload.html", form=form)
+
+
+# 画像処理系関数
+# 枠線の色をランダムに決定
+def make_color(labels):
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in labels]
+    color = random.choice(colors)
+    return color
+
+
+# 枠線を作成
+def male_line(result_image):
+    line = round(0.002 * max(result_image.shape[0:2])) * 1
+    return line
+
+
+# 四角形の枠線を画像に追記
+def draw_lines(c1, c2, result_image, line, color):
+    cv2.rectangle(result_image, c1, c2, color, thickness=line)
+    return cv2
+
+
+# 検知したテキストラベルを画像に追記
+def draw_texts(result_image, line, c1, cv2, color, labels, label):
+    display_txt = f"{labels[label]}"
+    font = max(line - 1, 1)
+    t_size = cv2.getTextSize(display_txt, 0, fontScale=line / 3, thickness=font)[0]
+    c2 = c1[0] + t_size[0], c[1] - t_size[1] - 3
+    cv2.rectangle(result_image, c1, c2, color, -1)
+    cv2.putText(
+        result_image,
+        display_txt,
+        (c1[0], c1[1] - 2),
+        0,
+        line / 3,
+        [255, 255, 255],
+        thickness=font,
+        lineType=cv2.LINE_AA,
+    )
+    return cv2
+
+
+# 画像検知を実行する本体
+def exec_detect(target_image_path):
+    # ラベルの読み込み
+    labels = current_app.config["LABELS"]
+    # 画像の読み込み
+    image = Image.open(target_image_path)
+    # 画像データをテンソル型の数値データへ
+    image_tensor = torchvision.transforms.functional.to_tensor(image)
+
+    # 学習済みモデルの読み込み
+    model = torch.load(Path(current_app.root_path, "detector", "model.pt"))
+    # モデルの推論モードに切り替え
+    model = model.eval()
+    # 推論実行
+    output = model([image_tensor])[0]
+
+    tags = []
+    result_image = np.array(image.copy())
+
+    # モデルが検知した各物体の分だけ画像に追記
+    for box, label, score in zip(output["boxes"], output["lavels"], output["scores"]):
+        if score > 0.5 and labels[label] not in tags:
+            # 上で作成した関数により、枠線を描画
+            color = make_color(labels)
+            line = make_line(result_image)
+            c1 = int(box[0], int(box[1]))
+            c2 = int(box[2], int(box[3]))
+            cv2 = draw_lines(c1, c2, result_image, line, color)
+            cv2 = draw_texts(result_image, line, c1, cv2, color, lavels, label)
+            tags.append(labels[label])
+
+    # 検知後の画像ファイル名を生成
+    detected_image_file_name = str(uuid.uuid4()) + ".jpg"
+    # 画像コピー先のパスを取得
+    detected_image_file_path = str(
+        Path(current_app.config["UPLOAD_FOLDER"], detected_image_file_name)
+    )
+    # 変換後の画像ファイルを保存先へコピー
+    cv2.imwrite(detected_image_file_path, cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR))
+
+    return tags, detected_image_file_name
+
+
+def save_detected_image_tags(user_image, tags, detected_image_file_name):
+    # 検知後画像の保存先パスをDBに保存する
+    user_image.image_path = detected_image_file_name
+    # 検知フラグをTに
+    user_image.is_detected = True
+    db.session.add(user_image)
+
+    # user_images_tagsレコードを作成
+    for tag in tags:
+        user_image_tag = UserImageTag(user_image_id=user_image.id, tag_name=tag)
+        db.session.add(user_image_tag)
+    db.session.commit()
+
+
+@dt.route("/detect/<string:image_id>", methods=["POST"])
+@login_required
+def detect(image_id):
+    # user_imageテーブルからレコードを取得する
+    user_image = db.session.query(UserImage).filter(UserImage.id == image_id).first()
+    if user_image is None:
+        flash("物体検知対象の画像が存在しません")
+        return redirect(url_for("detector.index"))
+
+    # 物体検知対象の画像パスを取得
+    target_image_path = Path(current_app.config["UPLOAD_FOLDER"], user_image.image_path)
+
+    tags, detected_image_file_name = exec_detect(target_image_path)
+
+    try:
+        save_detected_image_tags(user_image, tags, detected_image_file_name)
+    except SQLAlchemyError as e:
+        flash("物体検知処理でエラーが発生しました。")
+        db.session.rollback()
+        current_app.logger.error(e)
+        return redirect(url_for("detector.index"))
+
+    return redirect(url_for("detector.index"))
